ed838ad6fa4621766e9c0e34313c3e8010626945
kyrillos-nady <112775822+kyrillos-nady@users.noreply.github.com>
2023-10-26 13:48:14 +0300

    Update Script.py

--- Faker_data_project/Script.py
+++ Faker_data_project/Script.py
@@ -1,21 +1,3 @@
-"""
-How to Use:
-1- Run the program.
-2- Input the desired file name in the first field.
-3- Enter the number of rows you need in the second field.
-4- Click the "Generate" button.
-The generated file will be created next to the script code.
-==============================
-Description:
-This program is used to generate fake data, which can be employed for various purposes:
-- It can be used to generate a list of data for brute force attacks.
-- It can be used to generate sample data for testing.
-- It can be used to create demo datasets.
-- It can be used to generate data for training machine learning models.
-- It can be used for automating data entry tasks.
-Additionally, it can fulfill numerous other demands in your job.
-"""
-
 # Import necessary libraries
 import csv  # For working with CSV files
 from faker import Faker  # For generating fake data
